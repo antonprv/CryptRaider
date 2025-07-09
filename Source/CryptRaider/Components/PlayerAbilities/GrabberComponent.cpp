94eb414642ee0b1d92b667ccda0954d53332f1dd
anton.prv <anton.prv@yandex.com>
2025-07-09 11:30:56 +0400

    Fixed Mover Component general structure, finished building sphere trace part of Grabber Component

--- Source/CryptRaider/Components/PlayerAbilities/GrabberComponent.cpp
+++ Source/CryptRaider/Components/PlayerAbilities/GrabberComponent.cpp
@@ -1,10 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GrabberComponent.h"
-
-#include "Camera/CameraComponent.h"
 #include "Engine/World.h"
+
+#include "GrabberComponent.h"
+#include "Camera/CameraComponent.h"
+
+#include "DrawDebugHelpers.h"
+
 
 // Sets default values for this component's properties
 UGrabberComponent::UGrabberComponent()
@@ -24,6 +27,11 @@ void UGrabberComponent::BeginPlay()
 	
 	OwningActor = GetOwner();
 	OwnerCamera = OwningActor->FindComponentByClass<UCameraComponent>();
+	if (OwnerCamera == nullptr)
+	{
+		bIsValid = false;
+		UE_LOG(LogTemp, Error, TEXT("No Camera Component found in the owning Actor"))
+	}
 }
 
 
@@ -31,22 +39,43 @@ void UGrabberComponent::BeginPlay()
 void UGrabberComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
+
+	// DrawDebug(GrabDistabce, bIsDebugEnabled);
 }
 
-void UGrabberComponent::TraceGrabLineFromCameraView(float TraceDistance, FHitResult& OutHitResult, bool& OutIsHit) const
+void UGrabberComponent::TraceFromCamera(const float& TraceDistance, const float& SphereRadius, FHitResult& OutHitResult, bool& OutIsHit, const bool& Debug)
 {
-	if (GetWorld() == nullptr)
+	if (GetWorld() == nullptr || !bIsValid)
 	{
 		return;
 	}
+
+	if (Debug)
+	{
+		DrawDebug(TraceDistance, DebugLineThickness);
+	}
+
 	
 
-	// GetWorld()->LineTraceSingleByChannel(
-	// 	OutHitResult,
-	// 	OwnerCamera->GetForwardVector(),
-	// 	OwnerCamera->GetForwardVector() * TraceDistance,
-	// 	ECollisionChannel::
-	// 	)
+	GetWorld()->SweepSingleByChannel(
+		OutHitResult,
+		OwnerCamera->GetComponentLocation(),
+		OwnerCamera->GetComponentLocation() + OwnerCamera->GetForwardVector() * TraceDistance,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(SphereRadius));
+	
 }
 
+void UGrabberComponent::DrawDebug(const float& TraceDistance, const float& LineThickness) const
+{
+	DrawDebugLine(
+		GetWorld(),
+		OwnerCamera->GetComponentLocation(),
+		OwnerCamera->GetComponentLocation() + OwnerCamera->GetForwardVector() * TraceDistance,
+		FColor::Emerald,
+		false,
+		-1,
+		0,
+		LineThickness);
+}
--- Source/CryptRaider/Components/PlayerAbilities/GrabberComponent.h
+++ Source/CryptRaider/Components/PlayerAbilities/GrabberComponent.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "Components/ActorComponent.h"
+
 #include "GrabberComponent.generated.h"
 
 
@@ -28,13 +30,26 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, Category="Grabbing parameters", meta = (Tooltip = "Maximum grabbing distance"))
 	float GrabDistabce {2.f};
-	UFUNCTION(BlueprintCallable, Category= "Physics|Traces", meta = (Keywords = "Trace Line Hit"))
-	void TraceGrabLineFromCameraView (float TraceDistance, FHitResult & OutHitResult, bool & OutIsHit) const;
+	UPROPERTY(EditAnywhere, Category="Grabbing parameters", meta = (Tooltip = "Radius of the sphere trace for grabbing"))
+	float GrabbingRadius {30.f};
+	UPROPERTY(EditAnywhere, Category="Grabbing debug", meta = (Tooltip = "Toggle debug line draw on \\ off"))
+	bool bIsDebugEnabled {false};
+	UPROPERTY(EditAnywhere, Category="Grabbing debug",
+		meta = (Tooltip = "Thickness of the debug line, by default equal to the TraceFromCamera radius"))
+	float DebugLineThickness {GrabbingRadius};
 	
+	UFUNCTION(BlueprintCallable, Category= "Physics|Traces", meta = (Keywords = "Trace Line Hit",
+		Tooltip="Does a sphere trace from camera view, returns first object hit and hit result, can also draw debug line"))
+	void TraceFromCamera (const float& TraceDistance, const float& SphereRadius, FHitResult& OutHitResult, bool& OutIsHit, const bool& Debug = true);
+
+	UPROPERTY()
+	bool bIsValid {true};
 	UPROPERTY()
 	AActor* OwningActor {nullptr};
 	UPROPERTY()
 	UCameraComponent* OwnerCamera {nullptr};
+	UFUNCTION()
+	void DrawDebug(const float& TraceDistance, const float& LineThickness) const;
 };
