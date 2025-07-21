07d1acfa32dcc114a5ff6622932a3d1fbedad5da
anton.prv <anton.prv@yandex.com>
2025-07-21 18:12:28 +0400

    Forgot to add sound helper class

---
+++ Source/CryptRaider/GameSound/SoundHelpers.cpp
@@ -0,0 +1,7 @@
+// Made by Antosh (anton.prv), derivative work strictly prohibited, except for non-commercial use.
+
+
+#include "SoundHelpers.h"
+
+FName USoundHelpers::BigRoomTriggerName = FName("BigRoom");
+FName USoundHelpers::SmallRoomTriggerName = FName("SmallRoom");
---
+++ Source/CryptRaider/GameSound/SoundHelpers.h
@@ -0,0 +1,34 @@
+// Made by Antosh (anton.prv), derivative work strictly prohibited, except for non-commercial use.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SoundHelpers.generated.h"
+
+/**
+ * Just a collection of enum types.
+ * Unreal Engine wil only allow this in an empty UObject file,
+ * so this whole thing is a workaround
+ */
+
+/**
+ * Works closely with Metasounds, triggers sound variation based on room size
+ */
+
+UENUM()
+enum class ERoomSoundType
+{
+	BigRoom,
+	SmallRoom
+};
+
+UCLASS()
+class CRYPTRAIDER_API USoundHelpers : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static FName SmallRoomTriggerName;
+	static FName BigRoomTriggerName;
+};
